e7d462a08558d912b411b22339cfe6c99a4b0b78
collinewait <collinewait17@gmail.com>
2019-10-28 15:52:56 +0300

    create a todo item component

--- to-do-app.js
+++ to-do-app.js
@@ -1,3 +1,5 @@
+import './to-do-item.js';
+
 const template = document.createElement('template');
 template.innerHTML = `
 <style>
@@ -31,28 +33,28 @@ class TodoApp extends HTMLElement {
     this._shadowRoot = this.attachShadow({ mode: 'open' });
     this._shadowRoot.appendChild(template.content.cloneNode(true));
 
-    this.todoList = this._shadowRoot.querySelector('ul');
-    this.input = this._shadowRoot.querySelector('input');
+    this.$todoList = this._shadowRoot.querySelector('ul');
+    this.$input = this._shadowRoot.querySelector('input');
 
-    this.submitButton = this._shadowRoot.querySelector('button');
-    this.submitButton.addEventListener('click', this._addTodo.bind(this));
+    this.$submitButton = this._shadowRoot.querySelector('button');
+    this.$submitButton.addEventListener('click', this._addTodo.bind(this));
   }
 
   _addTodo() {
-    if (this.input.value.length > 0) {
-      this._todos.push({ text: this.input.value, checked: false });
+    if (this.$input.value.length > 0) {
+      this._todos.push({ text: this.$input.value, checked: false });
       this._renderTodoList();
-      this.input.value = '';
+      this.$input.value = '';
     }
   }
 
   _renderTodoList() {
-    this.todoList.innerHTML = '';
+    this.$todoList.innerHTML = '';
 
     this._todos.forEach((todo, index) => {
-      let todoItem = document.createElement('div');
-      todoItem.innerHTML = todo.text;
-      this.todoList.appendChild(todoItem);
+      let $todoItem = document.createElement('to-do-item');
+      $todoItem.setAttribute('text', todo.text);
+      this.$todoList.appendChild($todoItem);
     });
   }
 
